72bc53c75981215ea01b611084aa6664be78d616
Daniel Correa <daniel-correa95@hotmail.com>
2019-12-31 00:40:43 -0300

    Versão inicial curriculo

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+const path = require('path')
+
+const port = process.env.port || 3000
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', async (req, res) => {
+    res.render('curriculo')
+})
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log('Não foi possivel iniciar o servidor')
+    } else {
+        console.log('servidor rodando')
+    }
+})
